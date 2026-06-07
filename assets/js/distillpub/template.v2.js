266ad4be93f49b5496301caa19ede043a7377632
annadoesscience <50752714+annadoesscience@users.noreply.github.com>
2026-06-07 16:18:10 +0200

    Update template.v2.js - comment polyfill

--- assets/js/distillpub/template.v2.js
+++ assets/js/distillpub/template.v2.js
@@ -1406,7 +1406,9 @@ ${math}
   // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   // See the License for the specific language governing permissions and
   // limitations under the License.
-
+                
+  /*
+  
   function addPolyfill(polyfill, polyfillLoadedCallback) {
     console.debug('Runlevel 0: Polyfill required: ' + polyfill.name);
     const script = document.createElement('script');
@@ -1475,6 +1477,8 @@ ${math}
       return Polyfills._neededPolyfills;
     }
   }
+  
+  */
 
   // Copyright 2018 The Distill Template Authors
   //
